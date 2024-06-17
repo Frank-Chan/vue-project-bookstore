c365878f1709aa1b657eb0d750ad9b049e58716c
Frank-Chan <gisfeifly@gmail.com>
2024-06-17 18:35:54 +0800

    Feat:添加菜单组件

---
+++ src/components/Menu.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <ul>
+        <li>
+            <router-link to="/home">首页</router-link>
+        </li>
+        <li>
+            <router-link to="/newBooks">新书</router-link>
+        </li>
+        <li>
+            <a href="javascript:;">特价书</a>
+        </li>
+        <li>
+            <a href="javascript:;">教材</a>
+        </li>
+        <li>
+            <a href="javascript:;">试听教材</a>
+        </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+    name:"MenuPart",
+}
+</script>
+
+<style>
+
+</style>
