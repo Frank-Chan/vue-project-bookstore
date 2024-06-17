389cc0053b799f2b7a3d548b196bdc9b04df166d
Frank-Chan <gisfeifly@gmail.com>
2024-06-17 14:58:14 +0800

    Feat:配置服务端地址

--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ import store from "./store";
 import axios from "axios";
 import VueAxios from "vue-axios";
 
+axios.defaults.baseURL = '/api';    //对应vue.config.js中配置的proxy-api-target地址
+
 const app = createApp(App); //创建了Vue应用的实例
 app.use(router); //将Vue Router实例注册到Vue应用中
 app.use(store); //将Vuex Store实例注册到Vue应用中
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  //定义 Vue CLI 的配置选项。
+  transpileDependencies: true, //是否对依赖包进行编译
+  devServer: {
+    proxy: {
+      api: {
+        //这里的地址是后端数据接口的地址
+        target: "http://111.229.37.167/",
+        changeOrigin: true, //允许跨域
+      },
+    },
+  },
+});
