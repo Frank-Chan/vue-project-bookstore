25474f7bd60f4fecfd2341743cb39c5943eb63b6
Frank-Chan <gisfeifly@gmail.com>
2024-06-17 11:23:22 +0800

    引入vuex，创建Vuex Store实例，并注册到Vue应用中

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import store from "./store";
 
 const app = createApp(App); //创建了Vue应用的实例
 app.use(router); //将Vue Router实例注册到Vue应用中
+app.use(store); //将Vuex Store实例注册到Vue应用中
 app.mount("#app");
---
+++ src/store/index.js
@@ -0,0 +1,18 @@
+import { createStore } from "vuex";
+
+//创建了Vuex Store实例
+const store = createStore({
+  state: {
+    // 定义状态
+  },
+  mutations: {
+    // 定义修改状态的方法
+  },
+  actions: {
+    // 定义异步操作的方法
+  },
+  getters: {
+    // 定义计算属性
+  },
+});
+export default store;
