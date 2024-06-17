f24556737c46f7724a5e74e43aeb9bd692ba4694
Frank-Chan <gisfeifly@gmail.com>
2024-06-17 18:35:23 +0800

    Feat:添加购物车和页面头部组件

---
+++ src/components/Header.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <img src="@/assets/logo.png" />
+    <HeaderSearch />
+    <HeaderCart />
+    <sapn v-if="!user"
+      >你好，请！<router-link to="/login">登录</router-link> 免费
+      <router-link to="/register">注册</router-link>
+    </sapn>
+    <span v-else
+      >欢迎您，{{ user.username }},<a href="javascript:;" @click="logout"
+        >退出登录</a
+      ></span
+    >
+  </div>
+</template>
+
+<script>
+import HeaderSearch from "./HeaderSearch.vue";
+import HeaderCart from "./HeaderCart.vue";
+import { mapState, mapMutations } from "vuex";
+export default {
+  name: "HeaderPage",
+  components: {
+    HeaderSearch,
+    HeaderCart,
+  },
+  computed: {
+    // user模块带有命名空间
+    ...mapState("user", [
+      // 将this.user 映射为 this.$store.state.user.user
+      "user",
+    ]),
+  },
+  methods: {
+    logout() {
+      this.deleteUser();
+    },
+    // user模块带有命名空间
+    ...mapMutations("user", [
+      // 将this.deleteUser 映射为 this.$store.commit('user/deleteUser')
+      "deleteUser",
+    ]),
+  },
+};
+</script>
+
+<style>
+img {
+  width: 40px;
+  height: 30px;
+  position: absolute;
+  top: 0;
+  left: 0;
+}
+</style>
---
+++ src/components/HeaderCart.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="haderCart">
+    <a href="javascript:;" @click.prevent="handleCart">
+        <span>购物车{{ cartItemsCount }}</span>
+    </a>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+export default {
+    name:'HeaderCart',
+    computed:{
+       //cart模块带有命名空间
+       ...mapGetters('cart',{
+            //vuex的store中定义的一个getter,得到购物车中商品的数量
+            //将this.cartItemsCount映射为 this.$store.getters['cart/itemsCount']
+            cartItemsCount:'itemsCount'
+       })
+    },
+    methods:{
+        handleCart(){
+            this.$route.push("/cart");
+        }
+    }
+}
+</script>
+
+<style>
+.headerCart {
+    display: inline-block; 
+    position: relative;
+    text-align: center;
+    width: 100px;
+    height: 30px;
+    margin-top: 20px;
+    margin-bottom: 20px;
+    cursor: pointer;
+    margin-left: 20px;
+    background-color:red;
+    vertical-align: middle;
+}
+
+.headerCart a {
+    text-decoration: none;
+    color: white;
+    
+}
+.headerCart a > span{
+  position: absolute;
+  left: 2px;
+  right: 2px;
+  bottom: 5px;
+}
+</style>
