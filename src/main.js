9ec8a986cee28b01dda9db14cb3affd838459b5a
Frank-Chan <gisfeifly@gmail.com>
2024-06-17 11:04:09 +0800

    Feat:引入vue-router并进行基础配资

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
 
-createApp(App).mount('#app')
+const app = createApp(App); //创建了Vue应用的实例
+app.use(router); //将Vue Router实例注册到Vue应用中
+app.mount("#app");
